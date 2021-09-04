a380870a46581882413f6a415980a1792a58a414
Marco Amorim <marcoamorim95@hotmail.com>
2021-09-03 22:32:37 -0300

    implementing counter component

--- packages/counter/src/App.js
+++ packages/counter/src/App.js
@@ -1,7 +1,27 @@
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
-	return <div className="App">Hello React!</div>;
+	const [counter, setCounter] = useState(0);
+
+	const handleDecrement = () => {
+		if (counter > 0) {
+			setCounter(counter - 1);
+		}
+	};
+	const handeIncrement = () => {
+		setCounter(counter + 1);
+	};
+
+	return (
+		<div className="app">
+			<div className="counter">
+				<h1>Counter: {counter}</h1>
+				<button onClick={handeIncrement}>Increment</button>
+				<button onClick={handleDecrement}>Decrement</button>
+			</div>
+		</div>
+	);
 };
 
 export default App;
